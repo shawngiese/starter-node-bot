d777bc4ec5ac65af1c7c5778087ec2a2fade5163
shawngiese <shawngiese@gmail.com>
2016-06-22 01:01:21 +0200

    fixed user name

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ controller.on('bot_channel_join', function (bot, message) {
 })
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello ' + message.user + '.')
+  bot.reply(message, 'Hello <@' + message.user + '>.')
 })
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
